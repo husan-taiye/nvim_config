5cbbc6ebded2b2ed58728bc3626a7df10c89af38
qilu <qilu@xuetangx.com>
2023-05-23 15:26:34 +0800

    tmp save

--- init.lua
+++ init.lua
@@ -4,8 +4,8 @@ set.relativenumber = true
 set.clipboard = "unnamed"
 
 -- 在copy后高亮
-vim.api.nvim_create_autocmd({"TextYankPost"}, {
-	pattern = {"*"},
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	pattern = { "*" },
 	callback = function()
 		vim.highlight.on_yank({
 			timeout = 500,
@@ -22,26 +22,171 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", opt)
 vim.keymap.set("n", "<C-h>", "<C-w>h", opt)
 vim.keymap.set("n", "<C-j>", "<C-w>j", opt)
 vim.keymap.set("n", "<C-k>", "<C-w>k", opt)
+vim.keymap.set("n", "<Leader>[", "<C-o>", opt)
+vim.keymap.set("n", "<Leader>]", "<C-i>", opt)
 -- https://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
 vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
 vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true })
-
 -- lazy.nvim 安装：
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
+	-- base16
+	{
+		"RRethy/nvim-base16",
+		lazy = true,
+	},
+	-- which-key
+	{
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	},
+	-- telescope
+	{
+		cmd = "Telescope",
+		keys = {
+			{ "<leader>p",  ":Telescope find_files<CR>", desc = "find files" },
+			{ "<leader>P",  ":Telescope live_grep<CR>",  desc = "grep file" },
+			{ "<leader>rs", ":Telescope resume<CR>",     desc = "resume" },
+			{ "<leader>q",  ":Telescope oldfiles<CR>",   desc = "oldfiles" },
+		},
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.1',
+		-- or                              , branch = '0.1.1',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	-- persistence
+	-- persistence
+	{
+		"folke/persistence.nvim",
+		lazy = false,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		event = "VeryLazy",
+		dependencies = {
+			'neovim/nvim-lspconfig',
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-buffer',
+			'hrsh7th/cmp-path',
+			'hrsh7th/cmp-cmdline',
+			'hrsh7th/nvim-cmp',
+			'L3MON4D3/LuaSnip',
+		}
+	},
+	{
+		event = "VeryLazy",
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+		config = function()
+			require("mason").setup()
+		end
+	},
+	{
+		event = "VeryLazy",
+		"neovim/nvim-lspconfig",
+		dependencies = { 'williamboman/mason-lspconfig.nvim' }
+	},
+})
+
+-- 加载base16-tender
+vim.cmd.colorscheme("base16-tender")
+
+-- lsp
+local lspconfig = require('lspconfig')
+require("mason").setup()
+require("mason-lspconfig").setup()
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+		vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set('n', '<leader>wl', function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+		vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+		vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+		vim.keymap.set('n', '<leader>f', function()
+			vim.lsp.buf.format { async = true }
+		end, opts)
+	end,
+})
+-- python  pyright
+lspconfig.pyright.setup {}
+
+-- lua
+require("lspconfig").lua_ls.setup {}
+require("lspconfig").rust_analyzer.setup {}
+require("lspconfig").lua_ls.setup({
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim", "hs" },
+			},
+			workspace = {
+				checkThirdParty = false,
+				-- Make the server aware of Neovim runtime files
+				library = {
+					vim.api.nvim_get_runtime_file("", true),
+					"/Applications/Hammerspoon.app/Contents/Resources/extensions/hs/",
+					vim.fn.expand("~/lualib/share/lua/5.4"),
+					vim.fn.expand("~/lualib/lib/luarocks/rocks-5.4"),
+					"/opt/homebrew/opt/openresty/lualib",
+				},
+			},
+			completion = {
+				callSnippet = "Replace",
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 })
